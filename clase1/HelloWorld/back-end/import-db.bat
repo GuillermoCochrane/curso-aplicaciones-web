c65c70eec59aea44e561104c0060bc06eedf01ef
GuillermoCochrane <guilleac81@gmail.com>
2025-09-02 02:27:57 -0300

    Corregida carpeta de guardado de los scripts de importación

--- clase1/HelloWorld/back-end/import-db.bat
+++ clase1/HelloWorld/back-end/import-db.bat
@@ -3,7 +3,7 @@ echo ======================================
 echo Importando base de datos: helloworldapi
 echo ======================================
 
-"C:\Program Files\MongoDB\Server\6.0\bin\mongorestore.exe" --db helloworldapi ".\db_dumps\helloworldapi"
+"C:\Program Files\MongoDB\Server\6.0\bin\mongorestore.exe" --db helloworldapi "..\db_dumps\helloworldapi"
 
 if %ERRORLEVEL% neq 0 (
   echo [ERROR] Fallo el import de la base de datos.
--- clase1/HelloWorld/back-end/import-db.sh
+++ clase1/HelloWorld/back-end/import-db.sh
@@ -3,7 +3,7 @@ echo "======================================"
 echo "Importando base de datos: helloworldapi"
 echo "======================================"
 
-mongorestore --db helloworldapi ./db_dumps/helloworldapi
+mongorestore --db helloworldapi ../db_dumps/helloworldapi
 
 if [ $? -ne 0 ]; then
   echo "[ERROR] Fallo el import de la base de datos."
